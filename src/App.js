d6af574fe55e759fc7097881df3cc22768c62cf6
karya-mandiri <thiefkidx@gmail.com>
2021-07-06 19:59:11 +0700

    update 2

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ import {
   FormInputMessage,
   FormButton,
 } from "./ContactFormElements";
-// import { ToastContainer, toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { useForm } from "react-hook-form";
 
 const App = () => {
@@ -21,67 +21,67 @@ const App = () => {
     register,
     handleSubmit,
     formState: { errors },
+    watch,
   } = useForm();
+
+  console.log(watch("name"));
 
   //ini dengan cara ke google sheet
 
-  // const [data, setData] = useState({
-  //   name: "",
-  //   email: "",
-  //   phone: "",
-  //   message: "",
-  // });
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
 
-  // const { name, email, phone, message } = data;
+  const { name, email, phone, message } = data;
 
-  // const handleChange = (e) => {
-  //   setData({ ...data, [e.target.name]: e.target.value });
-  // };
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
 
-  // const onSubmit = async (e) => {
-  //   e.preventDefault();
+  const onSubmit = async (e) => {
+    console.log(e);
+    e.preventDefault();
 
-  //   try {
-  //     const response = await fetch(
-  //       "https://v1.nocodeapi.com/mfikri_/google_sheets/AbjCYjUIBePJWqnU?tabId=Sheet1",
-  //       {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //         body: JSON.stringify([
-  //           [name, email, phone, message, new Date().toLocaleString()],
-  //         ]),
-  //       }
-  //     );
-  //     await response.json();
-  //     setData({ ...data, name: "", email: "", phone: "", message: "" });
-  //     toast.success("Success send message", {
-  //       position: "top-center",
-  //       autoClose: 3000,
-  //       hideProgressBar: true,
-  //       closeOnClick: true,
-  //       pauseOnHover: false,
-  //       draggable: true,
-  //       progress: undefined,
-  //     });
-  //     console.log("success");
-  //   } catch (err) {
-  //     toast.error(err, {
-  //       position: "top-center",
-  //       autoClose: 3000,
-  //       hideProgressBar: true,
-  //       closeOnClick: true,
-  //       pauseOnHover: false,
-  //       draggable: true,
-  //       progress: undefined,
-  //     });
-  //     console.log(err);
-  //   }
-  // };
-
-  const onSubmit = (data) => {
-    console.log(data);
+    try {
+      const response = await fetch(
+        "https://v1.nocodeapi.com/mfikri_/google_sheets/AbjCYjUIBePJWqnU?tabId=Sheet1",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify([
+            [name, email, phone, message, new Date().toLocaleString()],
+          ]),
+        }
+      );
+      await response.json();
+      setData({ ...data, name: "", email: "", phone: "", message: "" });
+      toast.success("Success send message", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+      });
+      console.log("success");
+    } catch (err) {
+      toast.error(err, {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+      });
+      console.log(err);
+    }
   };
 
   return (
@@ -92,25 +92,25 @@ const App = () => {
           <FormContent>
             <Form
               action="#"
-              // name="stj-contact-form"
+              name="contact-form"
               onSubmit={handleSubmit(onSubmit)}
             >
               <FormH1>Let's Talk</FormH1>
               <FormLabel htmlFor="name">Name</FormLabel>
               <FormInput
                 type="text"
-                // name="name"
-                // value={name}
-                // onChange={handleChange}
+                name="name"
+                value={name}
+                onChange={handleChange}
                 {...register("name", { required: true })}
               />
               <ErrorWrap>{errors.name && "You must enter your name"}</ErrorWrap>
               <FormLabel htmlFor="email">Email</FormLabel>
               <FormInput
                 type="text"
-                // name="email"
-                // value={email}
-                // onChange={handleChange}
+                name="email"
+                value={email}
+                onChange={handleChange}
                 {...register("email", {
                   required: true,
                   pattern: {
@@ -125,24 +125,24 @@ const App = () => {
               <FormLabel htmlFor="phone">Phone Number</FormLabel>
               <FormInput
                 type="phone"
-                // name="phone"
-                // value={phone}
-                // onChange={handleChange}
+                name="phone"
+                value={phone}
+                onChange={handleChange}
                 {...register("phone", { required: false })}
               />
               <FormLabel htmlFor="message">Message</FormLabel>
               <FormInputMessage
                 type="message"
-                // name="message"
-                // value={message}
-                // onChange={handleChange}
+                name="message"
+                value={message}
+                onChange={handleChange}
                 {...register("message", { required: true })}
               />
               <ErrorWrap>
                 {errors.message && "You must enter your message"}
               </ErrorWrap>
               <FormButton type="submit">Send</FormButton>
-              {/* <ToastContainer /> */}
+              <ToastContainer />
             </Form>
           </FormContent>
         </FormWrap>
